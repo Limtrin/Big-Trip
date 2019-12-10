806d41aad22cb559bd2e048e69258678af3e9ab0
Limtrin <limonad13@gmail.com>
2019-12-10 14:08:06 +0400

    Личный проект: революция или эволюция? (Часть 2)

--- src/main.js
+++ src/main.js
@@ -7,38 +7,53 @@ import SortComponent from './components/sort.js';
 import EventEditComponent from './components/edit-event.js';
 import DayListComponent from './components/day-list.js';
 import EventComponent from './components/event.js';
+import NoEventsComponent from './components/no-events.js';
 import {generateEventList} from './mock/event.js';
 import {filterItems} from './mock/filter.js';
 import {menuItems} from './mock/site-menu.js';
 import {render, RenderPosition} from './utils.js';
 
-const EVENT_COUNT = 4;
+const EVENT_COUNT = 0;
 
 
-const renderEvent = (event) => {
+const renderEvent = (tripEventsElement, event) => {
+
+  const onEscKeyDown = (evt) => {
+    const isEscKey = evt.key === `Escape` || evt.key === `Esc`;
+
+    if (isEscKey) {
+      replaceEditToEvent();
+      document.removeEventListener(`keydown`, onEscKeyDown);
+    }
+  };
+
+  const replaceEditToEvent = () => {
+    tripEventsElement.replaceChild(eventComponent.getElement(), eventEditComponent.getElement());
+  };
+
+  const replaceEventToEdit = () => {
+    tripEventsElement.replaceChild(eventEditComponent.getElement(), eventComponent.getElement());
+  };
+
   const eventComponent = new EventComponent(event);
-  const eventEditComponent = new EventEditComponent(event);
-
   const editButton = eventComponent.getElement().querySelector(`.event__rollup-btn`);
   editButton.addEventListener(`click`, () => {
-    tripEventsElement.replaceChild(eventEditComponent.getElement(), eventComponent.getElement());
+    replaceEventToEdit();
+    document.addEventListener(`keydown`, onEscKeyDown);
   });
 
+
+  const eventEditComponent = new EventEditComponent(event);
   const editForm = eventEditComponent.getElement();
-  editForm.addEventListener(`submit`, () => {
-    tripEventsElement.replaceChild(eventComponent.getElement(), eventEditComponent.getElement());
-  });
+  editForm.addEventListener(`submit`, replaceEditToEvent);
 
   render(tripEventsElement, eventComponent.getElement(), RenderPosition.BEFOREEND);
 };
 
-
 const events = generateEventList(EVENT_COUNT);
 events.sort((a, b) => a.dateBegining - b.dateBegining);
 
-
 const siteRouteElement = document.querySelector(`.trip-main__trip-info`);
-render(siteRouteElement, new RouteComponent(events).getElement(), RenderPosition.BEFOREEND);
 render(siteRouteElement, new TotalComponent(events).getElement(), RenderPosition.BEFOREEND);
 
 const siteControlsElement = document.querySelector(`.trip-main__trip-controls`);
@@ -46,17 +61,23 @@ render(siteControlsElement.firstElementChild, new MenuComponent(menuItems).getEl
 render(siteControlsElement.lastElementChild, new FilterComponent(filterItems).getElement(), RenderPosition.AFTEREND);
 
 const siteMainElement = document.querySelector(`.trip-events`);
-render(siteMainElement, new SortComponent().getElement(), RenderPosition.BEFOREEND);
-render(siteMainElement, new BoardComponent().getElement(), RenderPosition.BEFOREEND);
+
+if (events.length === 0) {
+  render(siteMainElement, new NoEventsComponent().getElement(), RenderPosition.BEFOREEND);
+} else {
+  render(siteRouteElement, new RouteComponent(events).getElement(), RenderPosition.AFTERBEGIN);
+  render(siteMainElement, new SortComponent().getElement(), RenderPosition.BEFOREEND);
+  render(siteMainElement, new BoardComponent().getElement(), RenderPosition.BEFOREEND);
 
 
-const tripDayElement = siteMainElement.querySelector(`.trip-days`);
+  const tripDayElement = siteMainElement.querySelector(`.trip-days`);
 
-render(tripDayElement, new DayListComponent(events[0].dateBegining).getElement(), RenderPosition.BEFOREEND);
+  render(tripDayElement, new DayListComponent(events[0].dateBegining).getElement(), RenderPosition.BEFOREEND);
 
-const tripEventsElement = siteMainElement.querySelector(`.trip-events__list`);
+  const tripEventsElement = siteMainElement.querySelector(`.trip-events__list`);
 
-events
-    .forEach(
-        (event) => renderEvent(event)
-    );
+  events
+      .forEach(
+          (event) => renderEvent(tripEventsElement, event)
+      );
+}
